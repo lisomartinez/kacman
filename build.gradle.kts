e6c108b9ffcc0fae0705bcf529048340361838fe
Lisandro Martinez <lisandromartinez@gmail.com>
2021-01-11 18:02:48 -0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+
+group = "ar.coders"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val kotestVersion: String by project
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testRuntimeOnly("io.kotest:kotest-assertions-core:$kotestVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+application {
+    mainClassName = "MainKt"
+}
