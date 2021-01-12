bc41bbe017e810be68146abe1ff887cc7da9782d
Lisandro Martinez <lisandromartinez@gmail.com>
2021-01-12 11:36:33 -0300

    add ResultFormatter.kt formats respository, description and size

--- src/main/kotlin/format/ResultFormatter.kt
+++ src/main/kotlin/format/ResultFormatter.kt
@@ -3,7 +3,11 @@ package format
 class ResultFormatter {
     fun format(result: String): String {
         val agent = ColoringAgent()
-        return "Name: ${agent.colorName("curlie")}\n" + "Version: 1.6.0-1\n"
+        return "Name: ${agent.colorName("curlie")}\n" +
+                "Version: 1.6.0-1\n" +
+                "Description: The power of curl, the ease of use of httpie.\n" +
+                "Repository: ${agent.colorRepository("community")}\n" +
+                "Size: (863.1 KiB 2.6 MiB)"
     }
 
 }
--- src/test/kotlin/format/ResultFormatterTest.kt
+++ src/test/kotlin/format/ResultFormatterTest.kt
@@ -19,6 +19,27 @@ class ResultFormatterTest : StringSpec({
         val formattedResult: String = formatter.format(result)
         formattedResult shouldContain "Version: 1.6.0-1"
     }
+
+    "formats package description" {
+        val formatter: ResultFormatter = ResultFormatter()
+        val result = createPacmanSearchResultLines()
+        val formattedResult: String = formatter.format(result)
+        formattedResult shouldContain "Description: The power of curl, the ease of use of httpie."
+    }
+
+    "formats package repository" {
+        val formatter: ResultFormatter = ResultFormatter()
+        val result = createPacmanSearchResultLines()
+        val formattedResult: String = formatter.format(result)
+        formattedResult shouldContain "Repository: ${agent.colorRepository("community")}"
+    }
+
+    "formats package size" {
+        val formatter: ResultFormatter = ResultFormatter()
+        val result = createPacmanSearchResultLines()
+        val formattedResult: String = formatter.format(result)
+        formattedResult shouldContain "Size: (863.1 KiB 2.6 MiB)"
+    }
 })
 
 private fun createPacmanSearchResultLines(): String {
